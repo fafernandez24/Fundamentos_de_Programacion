93500ef05b6be7721cb1cdb42eec4b08ab64dd13
fafernandez24 <146951144+fafernandez24@users.noreply.github.com>
2023-10-04 21:02:05 -0400

    🖥️ Actividad 1 - 2%
    Traduzca a Python el pseudocódigo que hizo en la sesión anterior para calcular el área de un triángulo. Envíe tanto el pseudocódigo como el código python.

---
+++ calcular_area_triangulo.py
@@ -0,0 +1,12 @@
+#Codigo Python
+#Calcular el área de un triángulo
+
+#Declaración de variables
+
+b = float(input("Ingrese la base del triangulo: "))
+h = float(input("Ingrese la altura del triangulo: "))
+a = b * h/2
+
+#Resultado de la operación
+
+print("El área del triangulo es ", a)
---
+++ pseudocodigo_calcular_area_triangulo.psc
@@ -0,0 +1,14 @@
+Proceso calculareláreadeuntriangulo 
+	
+	Definir b1, h2 como Entero; 
+	
+	Escribir "ingrese b1:";
+	Leer b1;
+	Escribir "ingrese h2:" ; 
+	Leer h2; 
+	
+	Resultado = b1*h2/2
+	
+	Escribir (resultado);
+FinAlgoritmo
+
